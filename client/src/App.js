6fab912869e0e5ef05c824ad59f3c684e5c90da9
Jack Tomasik <jackedwardtomasik@gmail.com>
2022-06-24 14:15:17 -0700

    working now

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,9 @@ import React from "react";
 const App = () => {
     return(
         <div>
-            <h1>App</h1>
+            <h1>App is here</h1>
         </div>
     )
 }
+
+export default App;
