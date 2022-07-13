31d5a8c89062a786c5d349578c6e805d50c366ca
Jack Tomasik <jackedwardtomasik@gmail.com>
2022-07-12 17:17:40 -0700

    adding in redux and hooks issue with deprecated dependency

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+import { useDispatch } from 'react-redux';
 
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
@@ -8,6 +9,11 @@ import useStyles from './styles';
 
 const App = () => {
     const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch();
+    }, []);
     return(
         <Container maxwidth="lg">
             <AppBar className={classes.appBar} position="static" color="inherit">
