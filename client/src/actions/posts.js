bd3e1d40f244b5bf941155ccd707de29f2b87f53
Jack Tomasik <jackedwardtomasik@gmail.com>
2022-07-19 14:11:37 -0700

    fixing typo

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -23,7 +23,7 @@ export const createPost = (post) => async (dispatch) => {
     }
 }
 
-export const updatedPost = (id, post) => async (dispatch) => {
+export const updatePost = (id, post) => async (dispatch) => {
     try {
         const { data } = await api.updatePost(id, post);
 
