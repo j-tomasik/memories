d6debc2831be4fd89910e212407f0c0ac39d1b09
Jack Tomasik <jackedwardtomasik@gmail.com>
2022-07-21 16:59:55 -0700

    fixed update found typo

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -29,13 +29,13 @@ export const createPost = async (req, res) => {
 export const updatePost = async (req,res) => {
     const { id: _id } = req.params;
     const post = req.body;
-
+    debugger
     if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
 
     
     const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true });
 
     res.json(updatedPost);
-    console.log('post updated on backend!');
+    
 }
 
