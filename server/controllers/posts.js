a25d2ac429b3872409a0c3582fb8581bd810101e
Jack Tomasik <jackedwardtomasik@gmail.com>
2022-07-23 18:00:29 -0700

    fixed typo bug

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -42,7 +42,7 @@ export const updatePost = async (req,res) => {
 export const deletePost = async (req, res) => {
     const { id } = req.params;
 
-    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
 
     await PostMessage.findByIdAndRemove(id);
     
