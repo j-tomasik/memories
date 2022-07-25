497cda0ee67b6dd7ff672afe8c038fbd794490d3
Jack Tomasik <jackedwardtomasik@gmail.com>
2022-07-25 11:47:28 -0700

    tried to hide mongodb info didnt work

--- server/index.js
+++ server/index.js
@@ -2,13 +2,14 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 
 
 const app = express();
-
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -16,8 +17,11 @@ app.use(cors());
 
 app.use('/posts', postRoutes)
 
-const CONNECTION_URL = 'mongodb+srv://jtomasik:Edward5991!@cluster0.ks1kj.mongodb.net/?retryWrites=true&w=majority'
+
 const PORT = process.env.PORT || 5000;
+
+const stringURL = String(process.env.CONNECTION_URL)
+const CONNECTION_URL = 'mongodb+srv://jtomasik:Edward5991!@cluster0.ks1kj.mongodb.net/?retryWrites=true&w=majority'
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
