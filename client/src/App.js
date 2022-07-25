0aeb437429c9a2474bc395070b51b4fa346e7215
Jack Tomasik <jackedwardtomasik@gmail.com>
2022-07-25 16:05:01 -0700

    moved navbar to own component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+import { Container, Grow, Grid } from '@material-ui/core'
 import { useDispatch } from 'react-redux';
 
 import { getPosts } from './actions/posts'
@@ -7,6 +7,7 @@ import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import memories from './images/memories.png';
 import useStyles from './styles';
+import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
     const [currentId, setCurrentId] = useState(null);
@@ -18,6 +19,7 @@ const App = () => {
     }, [currentId, dispatch]);
     return(
         <Container maxwidth="lg">
+            <Navbar />
             <AppBar className={classes.appBar} position="static" color="inherit">
                 <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
                 <img className={classes.image} src={memories} alt="memories" height ="60" />
