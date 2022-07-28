eb0c3f9b90c4e0764ee20ab1c360b7787bb07f6d
Jack Tomasik <jackedwardtomasik@gmail.com>
2022-07-28 15:08:59 -0700

    building front end auth page

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import useStyles from './styles';
 
-export const Auth = () => {
-    const state = null;
+const Auth = () => {
+    const classes = useStyles();
+    const isSignup = false;
     
     return (
-        <div>
-            Auth
-        </div>
+        <Container component="main" maxwidth="xs">
+            <Paper className={classes.paper} elevation={3}>
+                <Avatar classsNmae={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography variant="h5">{isSignup ? 'Sign Up' : 'Sign In'}</Typography>
+            </Paper>
+        </Container>
     )
 }
+
+export default Auth;
