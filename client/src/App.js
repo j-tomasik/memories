7b5bc27dc20b0d62c42c326dab81acb3257007fd
Jack Tomasik <jackedwardtomasik@gmail.com>
2022-07-27 17:30:06 -0700

    refactoring app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,27 @@
-import React, { useState, useEffect } from "react";
-import { Container, Grow, Grid } from '@material-ui/core'
-import { useDispatch } from 'react-redux';
+import React from "react";
+import { Container } from '@material-ui/core'
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import { getPosts } from './actions/posts'
-import Posts from './components/Posts/Posts';
-import Form from './components/Form/Form';
-import memories from './images/memories.png';
-import useStyles from './styles';
+
 import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
-    const [currentId, setCurrentId] = useState(null);
-    const classes = useStyles();
-    const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(getPosts());
-    }, [currentId, dispatch]);
-    
+
+
     return(
-        <Container maxwidth="lg">
-            <Navbar />
-            <Home />
-        </Container>
+        <BrowserRouter>
+            <Container maxwidth="lg">
+                <Navbar />
+                <Switch>
+                    <Route path="/" exact component={Home}/>
+                    <Route path="/auth" exact component={Auth}/>
+                </Switch>
+                <Home />
+            </Container>
+        </BrowserRouter>
+        
     )
 }
 
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import { Container, Grow, Grid } from '@material-ui/core';
 
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
 import { useDispatch } from 'react-redux';
-import { getPosts } from './actions/posts'
+import { getPosts } from '../../actions/posts'
 
 
 export const Home = () => {
