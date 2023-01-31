3f8dc7f9489c0204d6611c541c94c9991ceae0af
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-01-30 21:30:46 -0800

    taking out extra component in app js

--- client/src/App.js
+++ client/src/App.js
@@ -30,7 +30,3 @@ const App = () => {
 
 export default App;
 
-{/* <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
-                <img className={classes.image} src={memories} alt="memories" height ="60" />
-            </AppBar> */}
