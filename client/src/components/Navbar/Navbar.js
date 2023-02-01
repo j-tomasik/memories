f1a46b5314c514e9e92b63a211702dd983559e01
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-02-01 13:18:03 -0800

    adding in google auth buttons to navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { AppBar,  Toolbar, Typography } from '@material-ui/core'
+import { AppBar,  Avatar,  Button,  Toolbar, Typography } from '@material-ui/core'
 import { useDispatch } from 'react-redux';
+import { GoogleLogin, googleLogout } from '@react-oauth/google';
 
 import useStyles from './styles'
 import memories from '../../images/memories.png';
@@ -9,7 +10,7 @@ import memories from '../../images/memories.png';
 const Navbar = () => {
     const classes = useStyles();
     
-
+    const user = null;
 
     return (
         <AppBar className={classes.appBar} position="static" color="inherit">
@@ -17,9 +18,21 @@ const Navbar = () => {
                 <Typography component={Link} to='/' className={classes.heading} variant="h2" align="center">Memories</Typography>
                 <img className={classes.image} src={memories} alt="memories" height ="60" />
             </div>
-        
-            
-    </AppBar>
+
+            <Toolbar className={classes.profile}>
+                {user ? (
+                    <div className={classes.profile}>
+                        //make sure to console.log the profile to check and make sure where can be rendered
+                        <Avatar className={classes.purple} alt={user.result.name} src={user.result.picture}>{user.result.family_name.charAt(0)}</Avatar>
+                        <Typography className={classes.userName} variant='h6'>{user.result.family_name}</Typography>
+                        <Button variant='contained' className={classes.logout} color='secondary' onClick={googleLogout}>Logout</Button>
+                    </div>
+
+                ) : (
+                    <Button component={Link} to='/auth' variant='contained' color='primary'>Sign In</Button>
+                )}
+            </Toolbar>    
+        </AppBar>
     )
 }
 
