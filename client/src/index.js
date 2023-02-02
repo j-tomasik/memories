4a8c6ff61d9e721c44d9903456e51119a1247a4b
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-02-02 11:47:43 -0800

    finalllyyy got google auth to work

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import { legacy_createStore as createStore, legacy_createStore} from 'redux'
 import {  applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
+import dotenv from 'dotenv';
 
 
 import reducers from './reducers'
@@ -13,13 +14,13 @@ import './index.css';
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 
-require('dotenv').config();
+dotenv.config();
 
 const store = legacy_createStore(reducers, compose(applyMiddleware(thunk)))
 
 ReactDOM.render(
 
-        <GoogleOAuthProvider client_id={process.env.GOOGLE_API_TOKEN}>
+        <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}>
             <Provider store={store}>           
                 <App />               
             </Provider>,
