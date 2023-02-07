c8f5af868d864cfde7eaef4cf5929a026561006e
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-02-07 12:51:03 -0800

    routes are now protected

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react';
+import React from 'react';
 import { Container } from '@material-ui/core'
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { createOrGetUser } from '../../api/index';
 import { useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
