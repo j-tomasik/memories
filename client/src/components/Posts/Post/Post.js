491fdd72e28dec191d0c80f4aee6a3ac9be49210
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-02-08 19:09:27 -0800

    saving progress

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -41,11 +41,14 @@ const Post = ({ post, setCurrentId }) => {
                 <Typography variant='h6'>{post.name}</Typography>
                 <Typography variant='body2'>{moment(post.createdAt).fromNow()}</Typography>
             </div>
-            <div className={classes.overlay2}>
+            {(user?.sub === post?.creator) && (
+                <div className={classes.overlay2}>
                 <Button style={{color: 'white'}} size="small" onClick={() => setCurrentId(post._id)}>
                     <MoreHorizIcon fontSize="medium"/>
                 </Button>
             </div>
+            )}
+
             <div className={classes.details}>
                 <Typography variant="body2" color="textSecondary">{post.tags.map((tag) => `#${tag} `)}</Typography>
             </div>
