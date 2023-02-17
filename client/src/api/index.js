64f596ca7a12f2f972ee9f708433ff006ef716a7
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-02-17 11:12:21 -0800

    moving to prod

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,7 +3,7 @@ import jwt_decode from 'jwt-decode'
 
 // const url = 'http://localhost:5000/posts';
 
-const API = axios.create({ baseURL: 'http://localhost:5000'});
+const API = axios.create({ baseURL: 'https://mems-app.herokuapp.com/'});
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('profile')) {
