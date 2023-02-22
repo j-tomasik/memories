d2b90441ce382cfba1225b72fb482079e1365d8b
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-02-21 16:48:07 -0800

    working great

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -4,7 +4,6 @@ import * as api from '../api/index.js';
 export const getPosts = (page) => async (dispatch) => {
     try {
         const { data } = await api.fetchPosts(page);
-        console.log('data', data);
 
         dispatch({ type: FETCH_ALL, payload: data });
     } catch (error) {
@@ -26,9 +25,11 @@ export const getPost = (id) => async (dispatch) => {
 
 export const createPost = (post) => async (dispatch) => {
     try {
+        dispatch({ type: START_LOADING });
         const { data } = await api.createPost(post);
 
         dispatch({ type: CREATE, payload: data });
+        dispatch({ type: END_LOADING});
     } catch (error) {
         console.log(error);
     }
@@ -67,9 +68,11 @@ export const likePost = (id) => async (dispatch) => {
 
 export const getPostsBySearch = (searchQuery) => async (dispatch) => {
     try {
+        dispatch({ type: START_LOADING });
         const { data: {data} } = await api.fetchPostsBySearch(searchQuery);
 
         dispatch({ type: FETCH_BY_SEARCH, payload: data })
+        dispatch({ type: END_LOADING});
     } catch (error) {
         console.log(error)
     }
