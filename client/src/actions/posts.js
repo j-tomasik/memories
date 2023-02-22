6715d89bc9caec28f9ce60338ea17418075900d2
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-02-22 12:25:33 -0800

    fixed reducer and actions for loading state starting rec posts

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -5,6 +5,7 @@ export const getPosts = (page) => async (dispatch) => {
     try {
         const { data } = await api.fetchPosts(page);
         dispatch({ type: FETCH_ALL, payload: data });
+        dispatch({ type: END_LOADING});
     } catch (error) {
         console.log(error)
     }
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -12,7 +12,6 @@ export default (state = { isLoading: true, posts: [] }, action) => {
             posts: action.payload.data,
             currentPage: action.payload.currentPage,
             numberOfPages: action.payload.numberOfPages,
-            isLoading: false,
             }
         case FETCH_BY_SEARCH:
             return { ...state, posts: action.payload}
