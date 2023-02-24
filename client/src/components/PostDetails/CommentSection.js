202ec3fa3aa0fea468c08534b9dd360593d3d9df
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-02-24 12:47:02 -0800

    working prototype for comment box

--- client/src/components/PostDetails/CommentSection.js
+++ client/src/components/PostDetails/CommentSection.js
@@ -4,11 +4,37 @@ import { useDispatch } from "react-redux";
 
 import useStyles from "./styles";
 
-const CommentSection = () => {
-    console.log('commetn section')
+const CommentSection = ({ post }) => {
+    console.log('post', post)
+    const classes = useStyles();
+    const [comments, setComments] = useState([1, 2, 3]);
+    const [comment, setComment] = useState("");
 
     return(
-        <h1>Comment Section</h1>
+        <div className={classes.commentsOuterContainer}>
+            <div className={classes.commentsInnerContainer}>
+                <Typography gutterBottom variant='h6'>Comments</Typography>
+                {comments.map((comment, index) => (
+                    <Typography key={index} gutterBottom variant="subtitle1">
+                        Comment {index}
+                    </Typography>
+                ))}
+            </div>
+            <div style={{width: '70%'}}>
+                    <Typography gutterBottom variant='h6'>Write a comment</Typography>
+                    <TextField
+                        fullWidth
+                        rows={4}
+                        variant="outlined"
+                        label="Comment"
+                        multiline
+                        value={comment}
+                        onChange={(e) => setComment(e.target.value)}
+                    />
+            </div>
+
+
+        </div>
     )
 }
 
